0e759628e57287355f1e92683da43d6a312703f9
ioannoid <io.a.skoul@gmail.com>
2022-03-25 08:58:27 -0400

    fdasf

--- plasma_bot/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $json = file_get_contents('php://input');
-    $data = json_decode($json);
-    echo $data->challenge;
-  }
-  else {
-    echo "hi";
-  }
